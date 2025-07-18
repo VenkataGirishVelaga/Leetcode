2eb8b7ce4554794eb7c7feb87f61fd7b2e703042
VELAGA VENKAT GIRISH <151516223+VenkataGirishVelaga@users.noreply.github.com>
2025-07-18 12:18:54 +0530

    Time: 7 ms (97.06%) | Memory: 8.9 MB (95.14%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -4,18 +4,16 @@ public:
         int n = s.size();
         int m = t.size();
 
-        vector<vector<double>> dp(n + 1, vector<double>(m + 1));
-        for(int i = 0; i <= n; i++) dp[i][0] = 1;
+        vector<double> prev(m + 1);
+        prev[0] = 1;
 
         for(int i = 1; i <= n; i++){
-            for(int j = 1; j <= m; j++){
+            for(int j = m; j > 0; j--){
                 if(s[i - 1] == t[j - 1]){
-                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
-                }else{
-                    dp[i][j] = dp[i - 1][j];
+                    prev[j] = prev[j - 1] + prev[j];
                 }
             }
         }
-        return (int)dp[n][m];
+        return (int)prev[m];
     }
 };
