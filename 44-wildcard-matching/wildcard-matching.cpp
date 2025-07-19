c11150fe377c2e84c12734700543c650d0fda3af
VELAGA VENKAT GIRISH <151516223+VenkataGirishVelaga@users.noreply.github.com>
2025-07-19 11:22:21 +0530

    Time: 26 ms (82.27%) | Memory: 11.3 MB (86.91%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -9,25 +9,30 @@ public:
     bool isMatch(string s, string p) {
         int n = s.size();
         int m = p.size();
-        vector<vector<bool>> dp(m + 1, vector<bool>(n + 1, false));
-        dp[0][0] = true;
+        vector<bool> prev(n + 1);
+        prev[0] = true;
+
+        // for(int i = 1; i <= m; i++){
+        //     if(p[i - 1] == '*'){
+        //         dp[i][0] = dp[i - 1][0];
+        //     }
+        // }
 
         for(int i = 1; i <= m; i++){
+            vector<bool> curr(n + 1, false);
             if(p[i - 1] == '*'){
-                dp[i][0] = dp[i - 1][0];
+                curr[0] = prev[0];
             }
-        }
-
-        for(int i = 1; i <= m; i++){
             for(int j = 1; j <= n; j++){
                 if(p[i - 1] == s[j - 1] || p[i - 1] == '?'){
-                    dp[i][j] = dp[i - 1][j - 1];
+                    curr[j] = prev[j - 1];
                 }else if(p[i - 1] == '*'){
-                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+                    curr[j] = prev[j] || curr[j - 1];
                 }
             }
+            prev = curr;
         }
 
-        return dp[m][n];
+        return prev[n];
     }
 };
