06f61685d787779155271191e31cbd631f2d1e53
VELAGA VENKAT GIRISH <151516223+VenkataGirishVelaga@users.noreply.github.com>
2025-07-19 18:43:06 +0530

    Time: 0 ms (100.00%) | Memory: 17.1 MB (70.07%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -2,15 +2,18 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        int aheadbuy = 0, aheadNotbuy = 0;
+        vector<int> after(2), curr(2);
         for(int i = n - 1; i >= 0; i--){
-           int currbuy = max(-prices[i] + aheadNotbuy, aheadbuy);
-           int currNotbuy = max(prices[i] + aheadbuy, aheadNotbuy);
-
-            aheadbuy = currbuy;
-            aheadNotbuy = currNotbuy;
-
+            for(int buy = 0; buy <= 1; buy++){
+                
+                if(buy){
+                    curr[1] = max(-prices[i] + after[0], after[1]);
+                }else{
+                    curr[0] = max(prices[i] + after[1], after[0]);
+                }
+            }
+            after = curr;
         }
-        return aheadbuy;
+        return after[1];
     }
 };
