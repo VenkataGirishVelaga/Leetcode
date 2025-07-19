5171826e0fba81773f468fe4bebedc2a2bb99bf2
VELAGA VENKAT GIRISH <151516223+VenkataGirishVelaga@users.noreply.github.com>
2025-07-19 11:14:35 +0530

    Time: 19 ms (92.04%) | Memory: 11.4 MB (86.79%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -9,23 +9,26 @@ public:
     bool isMatch(string s, string p) {
         int n = s.size();
         int m = p.size();
-        vector<vector<bool>> dp(m + 1, vector<bool>(n + 1, false));
-        dp[0][0] = true;
+        vector<bool> prev(n + 1,false);
+        prev[0] = true;
+
+        // for(int i = 1; i <= m; i++){
+        //     dp[i][0] = isvalid(p, i);
+        // }
 
         for(int i = 1; i <= m; i++){
-            dp[i][0] = isvalid(p, i);
-        }
-
-        for(int i = 1; i <= m; i++){
+            vector<bool> curr(n + 1, false);
+            curr[0] = isvalid(p, i);
             for(int j = 1; j <= n; j++){
                 if(p[i - 1] == s[j - 1] || p[i - 1] == '?'){
-                    dp[i][j] = dp[i - 1][j - 1];
+                    curr[j] = prev[j - 1];
                 }else if(p[i - 1] == '*'){
-                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+                    curr[j] = prev[j] || curr[j - 1];
                 }
             }
+            prev = curr;
         }
 
-        return dp[m][n];
+        return prev[n];
     }
 };
