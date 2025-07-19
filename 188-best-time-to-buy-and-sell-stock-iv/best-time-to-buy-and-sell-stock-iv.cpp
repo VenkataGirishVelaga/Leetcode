60ea9aa498ebd884af166d006a60cd4b6334df57
VELAGA VENKAT GIRISH <151516223+VenkataGirishVelaga@users.noreply.github.com>
2025-07-19 18:17:35 +0530

    Time: 7 ms (62.93%) | Memory: 14.6 MB (79.28%) - LeetSync

---
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxProfit(int k, vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<int>> after(2, vector<int>(2 * k + 1)), curr(2, vector<int>(2 * k + 1));
+
+        for(int i = n - 1;i >= 0; i--){
+            for(int buy = 0; buy <= 1; buy++){
+                for(int cap = 1; cap <= 2 * k; cap++){
+                    if(buy){
+                        curr[buy][cap] = max(-prices[i] + after[0][cap], after[1][cap]);
+                    }else{
+                        curr[buy][cap] = max(prices[i] + after[1][cap - 1], after[0][cap]);
+                    }
+                }
+            }
+            after = curr;
+        }
+        return after[1][k];
+    }
+};
