8265ef5880cddb94ad9cf3410b3c77366ac17f58
VELAGA VENKAT GIRISH <151516223+VenkataGirishVelaga@users.noreply.github.com>
2025-07-19 18:44:28 +0530

    Time: 0 ms (100.00%) | Memory: 17.1 MB (87.61%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -4,15 +4,11 @@ public:
         int n = prices.size();
         vector<int> after(2), curr(2);
         for(int i = n - 1; i >= 0; i--){
-            for(int buy = 0; buy <= 1; buy++){
-                
-                if(buy){
-                    curr[1] = max(-prices[i] + after[0], after[1]);
-                }else{
-                    curr[0] = max(prices[i] + after[1], after[0]);
-                }
-            }
-            after = curr;
+            curr[1] = max(-prices[i] + after[0], after[1]);
+        
+            curr[0] = max(prices[i] + after[1], after[0]);
+            
+            after = curr;         
         }
         return after[1];
     }
