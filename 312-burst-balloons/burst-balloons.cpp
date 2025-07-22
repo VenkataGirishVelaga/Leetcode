543761a18a9ea20b2d7e2dfd065bd2b69bb8716b
VELAGA VENKAT GIRISH <151516223+VenkataGirishVelaga@users.noreply.github.com>
2025-07-22 15:40:27 +0530

    Time: 92 ms (79.73%) | Memory: 15.3 MB (6.69%) - LeetSync

--- 312-burst-balloons/burst-balloons.cpp
+++ 312-burst-balloons/burst-balloons.cpp
@@ -6,8 +6,8 @@ public:
         nums.insert(nums.begin(), 1);
         vector<vector<long long>> dp(n + 2, vector<long long>(n + 2));
         for(int i = n; i >= 1; i--){
-            for(int j = 1; j <= n; j++){
-                if(i > j) continue;
+            for(int j = i; j <= n; j++){
+                //if(i > j) continue;
                 long long maxi = LLONG_MIN;
                 for(int k = i; k <= j; k++){
                     long long cost = 1LL * nums[i - 1] * nums[j + 1] * nums[k] + dp[i][k - 1] + dp[k + 1][j];
