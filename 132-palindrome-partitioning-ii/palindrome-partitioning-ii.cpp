7f2d41e942d1a68ba3f0f2ebd59a249c2f534d76
VELAGA VENKAT GIRISH <151516223+VenkataGirishVelaga@users.noreply.github.com>
2025-07-22 15:15:11 +0530

    Time: 1516 ms (39.55%) | Memory: 9.2 MB (93.57%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
@@ -1,27 +1,29 @@
 class Solution {
-public:    
+public:
+    bool ispal(int i, int j, const string& s) {
+        while(i < j) {
+            if(s[i] != s[j]) return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+
     int minCut(string s) {
         int n = s.size();
-        vector<int> dp(n + 1);
+        vector<int> dp(n + 1, 0); 
 
-        vector<vector<bool>> isPal(n, vector<bool>(n, false));
-        for (int i = n - 1; i >= 0; i--) {
-            for (int j = i; j < n; j++) {
-                if (s[i] == s[j] && (j - i <= 2 || isPal[i + 1][j - 1])) {
-                    isPal[i][j] = true;
-                }
-            }
-        }
-        for(int i = n - 1; i >= 0; i--){
+        for(int i = n - 1; i >= 0; i--) {
             int mini = INT_MAX;
-            for(int j = i; j < n; j++){
-                if(isPal[i][j]){
-                    int cost = 1 + dp[j+ 1];
+            for(int j = i; j < n; j++) {
+                if(ispal(i, j, s)) {
+                    int cost = 1 + dp[j + 1];
                     mini = min(mini, cost);
-                } 
+                }
             }
             dp[i] = mini;
         }
+
         return dp[0] - 1;
     }
 };
